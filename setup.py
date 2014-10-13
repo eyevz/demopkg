ee61e8dfe2a6c8a8cb1f50ce08e0e12f685f5e5c
Norman Ives <norman.ives@gmail.com>
2014-10-13 13:32:11 +0200

    Avoid false import error

--- setup.py
+++ setup.py
@@ -1,7 +1,16 @@
+from distutils.cmd import Command
 from os import path
 from setuptools import setup
 
-import bdist_wheel
+# The customised bdist_wheel relies on the wheel package, which will not always 
+# be installed during the first install of this package. This will not be a
+# problem unless the user is trying to build a distribution of this package
+# without first installing it, in a virtual environment that does not have the
+# wheel package installed.
+try:
+  from bdist_wheel import bdist_wheel
+except ImportError:
+  bdist_wheel = None
 
 here = path.abspath(path.dirname(__file__))
 
@@ -26,5 +35,5 @@ setup(
   license='Copyright Norman Ives 2014. All rights reserved.', 
   packages=['demopkg'],
   install_requires=install_requires,
-  cmdclass={'bdist_wheel': bdist_wheel.bdist_wheel}
+  cmdclass={'bdist_wheel': bdist_wheel}
 )
